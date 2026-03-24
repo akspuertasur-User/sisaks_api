f413b0d254c8edb5b301cb322833c1ef84a9fab6
akspuertasur-User <aks.puertasur@gmail.com>
2026-03-24 12:33:58 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,16 @@
 
+<?php
+$host = "centerbeam.proxy.rlwy.net";
+$port = 30012;
+$database = "sisaks_db";
+$user = "root";
+$password = "yAgbwVRFgBKVOkiTKXWoKvEprFhXdoFe";
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+?>
