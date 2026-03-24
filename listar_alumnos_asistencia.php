434220fb310660e5f308d76d1ce4741a7e8f2941
akspuertasur-User <aks.puertasur@gmail.com>
2026-03-24 12:36:15 -0300

    Update listar_alumnos_asistencia.php

--- listar_alumnos_asistencia.php
+++ listar_alumnos_asistencia.php
@@ -1,1 +1,32 @@
+<?php
+header('Content-Type: application/json');
+include 'conexion.php';
 
+$sql = "SELECT
+            a.id,
+            a.rut_alumno,
+            a.nombre_alumno,
+            a.escuela_id,
+            e.nombre_escuela,
+            a.total_asistencia,
+            a.activo
+        FROM alumnos a
+        INNER JOIN escuela e ON e.id = a.escuela_id
+        WHERE a.activo = 1
+        ORDER BY e.nombre_escuela ASC, a.nombre_alumno ASC";
+
+$result = $conn->query($sql);
+
+$alumnos = [];
+
+while ($row = $result->fetch_assoc()) {
+    $alumnos[] = $row;
+}
+
+echo json_encode([
+    "ok" => true,
+    "alumnos" => $alumnos
+]);
+
+$conn->close();
+?>
