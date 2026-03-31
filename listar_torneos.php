2da051fc5a0222857f0dcb21731631bd91c66405
akspuertasur-User <aks.puertasur@gmail.com>
2026-03-31 09:34:25 -0300

    Update listar_torneos.php

--- listar_torneos.php
+++ listar_torneos.php
@@ -1,1 +1,28 @@
+<?php
+header('Content-Type: application/json');
+include 'conexion.php';
 
+$sql = "SELECT id, nombre, fecha, lugar, descripcion
+        FROM Torneos
+        ORDER BY fecha DESC, id DESC";
+
+$resultado = $conn->query($sql);
+
+$torneos = [];
+
+if ($resultado) {
+    while ($fila = $resultado->fetch_assoc()) {
+        $torneos[] = $fila;
+    }
+
+    echo json_encode([
+        "ok" => true,
+        "torneos" => $torneos
+    ]);
+} else {
+    echo json_encode([
+        "ok" => false,
+        "mensaje" => "Error al listar torneos"
+    ]);
+}
+?>
