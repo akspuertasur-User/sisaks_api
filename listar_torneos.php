9e9b3c74f8657930635e5b1b7aeb88a02f1d906f
akspuertasur-User <aks.puertasur@gmail.com>
2026-03-31 13:36:25 -0300

    Update listar_torneos.php

--- listar_torneos.php
+++ listar_torneos.php
@@ -1,5 +1,10 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+ini_set('display_errors', 0);
+error_reporting(0);
+
 include 'conexion.php';
 
 $sql = "SELECT 
@@ -13,23 +18,25 @@ $sql = "SELECT
 
 $resultado = $conn->query($sql);
 
-$torneos = [];
-
-if ($resultado) {
-    while ($fila = $resultado->fetch_assoc()) {
-        $torneos[] = $fila;
-    }
-
-    echo json_encode([
-        "ok" => true,
-        "torneos" => $torneos
-    ]);
-} else {
+if ($resultado === false) {
     echo json_encode([
         "ok" => false,
         "mensaje" => "Error al listar torneos"
     ]);
+    $conn->close();
+    exit;
 }
+
+$torneos = [];
+
+while ($fila = $resultado->fetch_assoc()) {
+    $torneos[] = $fila;
+}
+
+echo json_encode([
+    "ok" => true,
+    "torneos" => $torneos
+]);
 
 $conn->close();
 ?>
